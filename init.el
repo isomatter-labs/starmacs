658718c5664566af3738a26f0a48a6d1cb8ed468
Cooper Healy <cooper.healy@garmin.com>
2021-03-29 11:21:12 -0500

    Add package auto updating and adhere to styleguide
    Change-Id: Ia368a4d836011aab6b9657cba45a99d3f29ac4d5

--- init.el
+++ init.el
@@ -1,7 +1,6 @@
 (require 'org)
 (org-babel-load-file
- (expand-file-name "config.org"
-		   user-emacs-directory))
+ (expand-file-name "config.org" user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -10,14 +9,14 @@
  '(custom-safe-themes
    '("77113617a0642d74767295c4408e17da3bfd9aa80aaa2b4eeb34680f6172d71a" default))
  '(package-selected-packages
-   '(company-rtags irony-server irony-eldoc flycheck-irony company-irony irony flycheck py-autopep8 haskell-mode cider lispy slime eglot magithub magit counsel ivy company-quickhelp company dumb-jump projectile olivetti mmm-mode pretty-mode rainbow-delimiters dashboard doom-modeline org-bullets doom-themes use-package)))
+   '(auto-package-update org helpful ivy-prescient ivy-rich company-rtags irony-server irony-eldoc flycheck-irony company-irony irony flycheck py-autopep8 haskell-mode cider lispy slime eglot magithub magit counsel ivy company-quickhelp company dumb-jump projectile olivetti mmm-mode pretty-mode rainbow-delimiters dashboard doom-modeline org-bullets doom-themes use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:foreground "#5B6268" :slant italic :family "IBM Plex Mono"))))
+ '(font-lock-comment-face ((t (:foreground "#5B6268" :weight light :slant italic :family "Iosevka"))))
  '(whitespace-newline ((t (:foreground "#525252"))))
  '(whitespace-space ((t (:foreground "#525252"))))
- '(whitespace-tab ((t (:foreground "#525252")))))
+ '(whitespace-tab ((t (:foreground "#E06C75")))))
 (put 'downcase-region 'disabled nil)
