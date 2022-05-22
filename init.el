fa5df1137d3759f4ef26aa666588b7df5627e486
M Cooper Healy <m.cooper.healy@gmail.com>
2022-05-22 12:57:11 -0500

    Add GitLens-like line annotations for git

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("77113617a0642d74767295c4408e17da3bfd9aa80aaa2b4eeb34680f6172d71a" default))
- '(package-selected-packages '(use-package)))
+ '(package-selected-packages '(blamer use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
