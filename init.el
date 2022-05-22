eef2c42122f439acf78d6ef459f65ac28cd09d19
M Cooper Healy <m.cooper.healy@gmail.com>
2022-05-22 14:08:42 -0500

    Add modes for working on TypeScript

--- init.el
+++ init.el
@@ -15,12 +15,13 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("77113617a0642d74767295c4408e17da3bfd9aa80aaa2b4eeb34680f6172d71a" default))
- '(package-selected-packages '(blamer use-package)))
+ '(package-selected-packages '(prettier-js rjsx-mode flyckeck tide blamer use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blamer-face ((t :foreground "#7a88cf" :background nil :height 140 :italic t)))
  '(font-lock-comment-face ((t (:foreground "#5B6268" :weight light :slant italic :family "Iosevka"))))
  '(whitespace-newline ((t (:foreground "#525252"))))
  '(whitespace-space ((t (:foreground "#525252"))))
