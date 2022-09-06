8335ceac37345ab91cb1e24239bf20cfd6204a1a
M Cooper Healy <m.cooper.healy@gmail.com>
2022-09-05 21:22:10 -0500

    lsp-mode and evil-mode

--- init.el
+++ init.el
@@ -16,15 +16,15 @@
  '(custom-safe-themes
    '("77113617a0642d74767295c4408e17da3bfd9aa80aaa2b4eeb34680f6172d71a" default))
  '(package-selected-packages
-   '(straight prettier-js rjsx-mode flyckeck tide blamer use-package)))
+   '(evil-goggles dired-sidebar vscode-icon neotree straight prettier-js rjsx-mode flyckeck tide blamer use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blamer-face ((t :foreground "#7a88cf" :background nil :height 140 :italic t)))
- '(font-lock-comment-face ((t (:foreground "#5B6268" :weight light :slant italic :family "Iosevka"))))
  '(whitespace-newline ((t (:foreground "#525252"))))
  '(whitespace-space ((t (:foreground "#525252"))))
  '(whitespace-tab ((t (:foreground "#E06C75")))))
 (put 'downcase-region 'disabled nil)
+(evil-mode 1)
