b3fd53c8e06c3d5486a4f6f8f0ba78fd3a7d2a29
M Cooper Healy <m.cooper.healy@gmail.com>
2022-10-02 10:03:39 -0500

    Revert "Ivy+Counsel => Vertico + Consult"
    This reverts commit facd541af9c26fdd9c1a818b1d497ca9de2a579b.

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
    '("77113617a0642d74767295c4408e17da3bfd9aa80aaa2b4eeb34680f6172d71a" default))
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(why-this which-key visual-fill-column typescript-mode treemacs-projectile treemacs-magit tree-sitter-langs solaire-mode sbt-mode rainbow-delimiters quelpa-use-package py-autopep8 prettier-js org-bullets mmm-mode magithub lsp-ui lsp-metals lsp-julia lsp-ivy julia-snail ivy-rich helpful general forge flycheck doom-themes doom-modeline discover diff-hl dashboard company-box command-log-mode auto-package-update all-the-icons)))
+   '(why-this which-key visual-fill-column typescript-mode treemacs-projectile treemacs-magit tree-sitter-langs solaire-mode sbt-mode rainbow-delimiters quelpa-use-package py-autopep8 prettier-js org-bullets mmm-mode magithub lsp-ui lsp-metals lsp-julia lsp-ivy julia-snail ivy-rich helpful general forge flycheck doom-themes doom-modeline discover diff-hl dashboard counsel-projectile company-box command-log-mode auto-package-update all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
